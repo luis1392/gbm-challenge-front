ae74e9642239c3885d0d5a1c12e5c53502d07879
luis1392 <luis.enri1392@gmail.com>
2020-09-09 22:26:08 -0500

    add new route /precios-y-cotizaciones

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 
 // Import pages
 import Home from "../../pages/Home";
+import PriceQuotes from "../../pages/PriceQuotes";
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/precios-y-cotizaciones" component={PriceQuotes} />
     </Switch>
   );
 };
