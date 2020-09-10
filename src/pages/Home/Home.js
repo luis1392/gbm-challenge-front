214a6b05e6c51fafdcc1bc327b28ec8474fa2da1
luis1392 <luis.enri1392@gmail.com>
2020-09-09 22:23:48 -0500

    initial home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Layout from "../../components/Layout";
+import { Container } from "reactstrap";
+// import WidgetIpc from "../../components/WidgetIpc";
+
 const Home = () => {
-  return <div>Pagina home</div>;
+  return (
+    <Layout>
+      <h1>Home page</h1>
+    </Layout>
+  );
 };
 
 export default Home;
