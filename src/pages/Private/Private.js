89c5e02df21edb426e178622777736986fc3dfa4
luis1392 <luis.enri1392@gmail.com>
2020-09-10 20:53:12 -0500

    create private page

---
+++ src/pages/Private/Private.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+import { Container } from "reactstrap";
+
+const Private = () => {
+  return (
+    <Layout>
+      <Container>
+        <h1>Has iniciado sesión</h1>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Private;
