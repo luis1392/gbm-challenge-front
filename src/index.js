21e36580c2d9171dd35df5245f493a3e48814887
luis1392 <luis.enri1392@gmail.com>
2020-09-10 21:09:36 -0500

    config init redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,23 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+// Redux
+import { Provider } from "react-redux";
+import makeStore from "./redux/makeStore";
+
 // add bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css"; // global css
 
+const store = makeStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
